390518a5865d8812e80dc69038a7fb67e35fd1ef
Ujjwal Kirti <kirti.chargers@gmail.com>
2025-11-26 01:00:43 +0530

    add: mime type

--- build-project/azureBlob.js
+++ build-project/azureBlob.js
@@ -1,4 +1,5 @@
 import { createReadStream } from "fs";
+import mime from "mime-types";
 
 class AzureBlobService {
     constructor(blobServiceClient) {
@@ -10,7 +11,7 @@ class AzureBlobService {
         const containerClient = this.blobServiceClient.getContainerClient("build-outputs");
         const blockBlobClient = containerClient.getBlockBlobClient(blobPath);
         const fileStream = createReadStream(filePath);
-        await blockBlobClient.uploadStream(fileStream);
+        await blockBlobClient.uploadStream(fileStream, { blobHTTPHeaders: { blobContentType: mime.lookup(filePath) || "application/octet-stream" } });
     }
 }
 
